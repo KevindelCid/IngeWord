28385ee3dcef206f33d8a8c33555eb927d919d88
Kevin <Kevindelcid731@gmail.com>
2021-05-17 02:47:43 -0600

    se agregó el fragment de la introducción

--- app/src/main/java/com/ingeword/main_proyect.java
+++ app/src/main/java/com/ingeword/main_proyect.java
@@ -2,12 +2,15 @@ package com.ingeword;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.ingeword.fragments.introFragment;
 
 public class main_proyect extends AppCompatActivity {
 
@@ -25,6 +28,7 @@ public class main_proyect extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
                 if(item.getItemId() == R.id.menu_intro){
+                    showSelectedFragment(new introFragment());
                     Toast.makeText(getApplicationContext(),
                         "Estas en la ventana de la introduccion", Toast.LENGTH_SHORT).show(); }
                 if(item.getItemId() == R.id.menu_body){
@@ -42,5 +46,12 @@ public class main_proyect extends AppCompatActivity {
             }
         });
 
+
+
+    }
+    private void showSelectedFragment(Fragment fragment){
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment)
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE).commit();
     }
 }
